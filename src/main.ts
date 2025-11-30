8a2511f4ced187fdb8b0ec6c61ccc5c9ce3eb334
Phạm Trường <phamtruong04112004@gmail.com>
2025-11-30 19:22:39 +0700

    feat: tracking preview

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,18 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import '@angular/localize/init';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+import reportWebVitals from '@vercel/analytics';
+import { Router, NavigationEnd } from '@angular/router';
+
+bootstrapApplication(AppComponent, appConfig)
+    .then((appRef) => {
+        reportWebVitals.inject({});
+
+        const router = appRef.injector.get(Router);
+        router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                reportWebVitals.track('pageview', { url: event.urlAfterRedirects });
+            }
+        });
+    })
+    .catch((err) => console.error(err));
